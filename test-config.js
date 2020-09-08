fe8f23585603bafdbda1e4fa08cf4d33dbbb135a
Tu Huynh <tuhuynh@Tus-MacBook-Pro.local>
2020-09-08 15:41:04 -0500

    lesson 03

---
+++ test-config.js
@@ -0,0 +1,15 @@
+exports.config = {
+    runner: 'local',
+    port: 4723,
+    host: 'localhost',
+    path: '/wd/hub',
+    logLevel: 'info',
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        require: ['@babel/register'],
+        timeout: 600000
+    },
+    maxInstances: 1,
+    sync: true
+}
